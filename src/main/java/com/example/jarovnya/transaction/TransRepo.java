6fc87a542cdd8e2aa602d9fb63d874526e6a4543
jehaierys <kawaysempai@gmail.com>
2024-10-04 16:19:18 +0500

    fix mistakes

--- src/main/java/com/example/jarovnya/transaction/TransRepo.java
+++ src/main/java/com/example/jarovnya/transaction/TransRepo.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TransRepo extends JpaRepository<Long, Transaction> {
+public interface TransRepo extends JpaRepository<Transaction, Long> {
 
 }
--- src/main/java/com/example/jarovnya/transaction/TransService.java
+++ src/main/java/com/example/jarovnya/transaction/TransService.java
@@ -10,7 +10,7 @@ import java.util.Optional;
 public class TransService {
     private final TransRepo transRepo;
 
-    public Optional<Transaction> getTransaction(Long id) {
-        return transRepo.getReferrenceById(id);
+    public Transaction getTransaction(Long id) {
+        return transRepo.getReferenceById(id);
     }
 }
