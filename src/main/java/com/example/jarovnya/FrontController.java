4ad9d8ae1a54c008b5aa8d2cbe00715a58fb8b7c
jehaierys <kawaysempai@gmail.com>
2024-10-09 21:03:28 +0500

    update: set right source links

---
+++ src/main/java/com/example/jarovnya/FrontController.java
@@ -0,0 +1,17 @@
+package com.example.jarovnya;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class FrontController {
+
+    @GetMapping("/home")
+    public String home() {
+        return "main";
+    }
+    @GetMapping("/catalog")
+    public String catalog() {
+        return "catalog";
+    }
+}
